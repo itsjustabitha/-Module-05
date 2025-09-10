9131a5a5a75beaa6814327c32d3e6151cccf2182
Tabitha Miller <155591728+itsjustabitha@users.noreply.github.com>
2025-09-10 14:11:13 -0500

    Lab5.1 Finished.

--- index.js
+++ index.js
@@ -6,36 +6,57 @@ const express = require('express')
 
 //create an app using the express package
 // express() is a function that creates an Express application object.
-const app = express();
 const app1 = express();
 const app2 = express();
 
 // set the port to 3000 (http://localhost:3000)
 const port = 3000
+const port2 = 3001
 
-app.get('/', (req, res) => {
+app1.get('/', (req, res) => {
 res.send('Hello World!')
 })
 
 // callback (() => { ... }) runs once the server starts.
-app.listen(port, () => {
-console.log(`Example app listening at 
+app1.listen(port, () => {
+console.log(`Lab1 app listening at 
 http://localhost:${port}`);
 });
 
 
+app2.get('/', (req, res) => {
+  res.send('Hello World from app2!')
+  })
+  
 
-// Example app listening at http://localhost:3000 - Hello World!
+  app2.listen(port2, () => {
+  console.log(`Example app listening at 
+  http://localhost:${port2}`);
+  });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-    })
 
-app.get('/test', (req, res) => {
-    res.send('This is a test')
-    })
 
-app.listen(port, () => {
-    console.log(`Example app listening at
-    http://localhost:${port}`)
-    })
+
+
+
+
+
+
+
+
+
+
+// // Example app listening at http://localhost:3000 - Hello World!
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//     })
+
+// app.get('/test', (req, res) => {
+//     res.send('This is a test')
+//     })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening at
+//     http://localhost:${port}`)
+//     })
